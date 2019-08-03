90e29b26bd0d6f5b5c358f6c1739750f9cb86da9
harell <harel@loostiger.com>
2019-08-03 16:47:50 +1200

    doc update

--- tests/component-tests/test-Ingest.R
+++ tests/component-tests/test-Ingest.R
@@ -1,6 +1,6 @@
-context("component test for Ingest interface")
+context("component test for Ingest's interface")
 
-test_that("Ingest fulfils its datasets assumptions", {
+test_that("Ingest interface method signatures", {
     expect_class(db <- Ingest$new(path = tempdir()), "Ingest")
     expect_an_empty_data.frame(db$historical_data)
     expect_an_empty_data.frame(db$new_data)
