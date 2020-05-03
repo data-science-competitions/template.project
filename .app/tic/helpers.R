903c57b5ee4fb49a43a381c51ef50a49c684c86e
harell <harel@loostiger.com>
2020-05-03 19:29:48 +1200

    code coverage report

--- .app/tic/helpers.R
+++ .app/tic/helpers.R
@@ -48,8 +48,9 @@ deploy_shiny <- function(stage){
 report_covr <- function(stage){
     if(!tic::ci_on_travis()) return(stage)
     stage %>%
+        add_step(step_message(c(sep(), "\n## Code Coverage Report", sep()))) %>%
         add_code_step(Sys.setenv(TESTTHAT = "true")) %>%
-        add_code_step(print(covr::codecov(quiet = FALSE))) %>%
+        add_code_step(covr::codecov(quiet = FALSE)) %>%
         add_code_step(Sys.unsetenv("TESTTHAT"))
 }
 
--- tic.R
+++ tic.R
@@ -23,7 +23,7 @@ if(is_master_branch() | is_hotfix_branch()){
     get_stage("script") %>% build_steps() %>% test_suite_steps()
 
 } else if (is_feature_branch()){
-    get_stage("script") %>% test_suite_steps() %>% report_covr()
+    get_stage("script") %>% test_suite_steps()
 }
 
 # Stage: After Success ----------------------------------------------------
